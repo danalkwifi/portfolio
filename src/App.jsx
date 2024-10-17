a33580e3744ad60b5f0b881d8da0e0e435a594f4
dalkwifi <dalkwifi@uwo.ca>
2024-10-17 11:16:57 -0400

    Installed assests and set up home page background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Navbar } from "./components/Navbar";
+
 const App = () => {
-  return(
-    <div>
-      <h1 className = "text-3xl font-bold underline">
-        Hello World!
-      </h1>
+  return (
+    <div className="overflow-x-hidden text-stone-300 antialiased">
+      <div className="fixed inset-0 -z-10">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
