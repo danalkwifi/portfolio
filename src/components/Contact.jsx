a258611da228a46dcd7e33a456fd1969c2cc55b2
dalkwifi <dalkwifi@uwo.ca>
2024-10-17 15:03:17 -0400

    Added animation to seach section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,36 @@
 import { CONTACT } from "../constants"
+import { motion } from "framer-motion"
 
 const Contact = () => {
   return (
     <div className="border-t border-stone-900 pb-20">
-        <h2 className="my-20 text-center text-4xl text-purple-700">Get in touch</h2>
+        <motion.h2 
+            whileInView={{opacity: 1, y:0}}
+            initial= {{opacity: 0, y:-100}}
+            transition={{duration: 0.5}}
+            className="my-20 text-center text-4xl text-purple-700">Get in touch</motion.h2>
         <div className="text-center tracking-tighter text-black">
-            <p className="my-4">
+            <motion.p 
+                whileInView={{opacity: 1, x:0}}
+                initial={{opacity: 0, x:-100}}
+                transition={{duration: 1}}
+                className="my-4">
                 {CONTACT.address}
-            </p>
-            <p className="my-4">
+            </motion.p>
+            <motion.p 
+                whileInView={{opacity: 1, x:0}}
+                initial={{opacity: 0, x:-100}}
+                transition={{duration: 1}}
+                className="my-4">
                 {CONTACT.phoneNo}
-            </p>
-            <p href="#" className="border-b">
+            </motion.p>
+            <motion.p 
+                whileInView={{opacity: 1, x:0}}
+                initial={{opacity: 0, x:-100}}
+                transition={{duration: 1}}
+                href="#" className="border-b">
                 {CONTACT.email}
-            </p>
+            </motion.p>
         </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,24 @@
 import profilePic from "../assets/danalkwifiProfile.jpg"
 import { HERO_CONTENT } from "../constants"
 import './customStyles.css'
+import { motion } from 'framer-motion'
+
+const containerVariants = {
+    hidden:{opacity: 0, x: -100},
+    visible:{
+        opacity: 1, 
+        x: 0,
+        transition: {
+            duration: 0.5, 
+            staggerChildren: 0.5,
+        }
+    }
+}
+
+const childVariations = {
+    hidden: { opacity: 0, x: -100},
+    visible: {opacity: 1, x: 0, transition: { duration: 0.5 }} 
+}
 
 const Hero = () => {
   return (
@@ -20,24 +38,36 @@ const Hero = () => {
             <div className="flex flex-wrap lg:flex-row-reverse">
             <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:p-8">
-                    <img src={profilePic} alt="Dana Al-Kwifi" className="border order-stone-900 rounded-3xl" /> 
+                    <motion.img src={profilePic} alt="Dana Al-Kwifi" className="border order-stone-900 rounded-3xl" width={650}  height={650}
+                        initial={{x:100, opacity:0}} animate={{x:0, opacity: 1}} transition={{duration: 1, delay: 1.5}} /> 
                 </div>
             </div>
             <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start">
-                    <h2 className="pt-6 pb-3 text-5xl tracking-tighter lg:text-8xl text-purple-900">Dana Al-Kwifi</h2>
-                    <span className="bg-gradient-to-r from-purple-700 to-purple-500 bg-clip-text text-3xl tracking-tighter text-transparent">
+                <motion.div 
+                    initial="hidden"
+                    animate="visible"
+                    variants={containerVariants}
+                    className="flex flex-col items-center lg:items-start">
+                    <motion.h2
+                        variants={childVariations} 
+                        className="pt-6 pb-3 text-5xl tracking-tighter lg:text-8xl text-purple-900">Dana Al-Kwifi</motion.h2>
+                    <motion.span 
+                        variants={childVariations}
+                        className="bg-gradient-to-r from-purple-700 to-purple-500 bg-clip-text text-3xl tracking-tighter text-transparent">
                         Web Developer
-                    </span>
-                    <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter text-stone-500">
+                    </motion.span>
+                    <motion.p 
+                        variants={childVariations}
+                        className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter text-stone-500">
                         {HERO_CONTENT}
-                    </p>
-                    <a href="/resume.pdf"
+                    </motion.p>
+                    <motion.a href="/resume.pdf"
                         target="_blank"
                         rel="noopener noreferrer"
                         download
-                        className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full p-4 text-sm text-white mb-10 relative z-20">Download Resume</a>
-                </div>
+                        className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full p-4 text-sm text-white mb-10 relative z-20"
+                        variants={childVariations}>Download Resume</motion.a>
+                </motion.div>
             </div>
             </div>
         
